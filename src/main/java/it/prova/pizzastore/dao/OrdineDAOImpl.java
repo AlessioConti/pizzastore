27b187e4a5e14ac31456efa8e4e45d4da8687c91
AlessioConti <alessioconti02@yahoo.it>
2022-06-02 23:06:48 +0200

    Implementazione findByExample Ordine

--- src/main/java/it/prova/pizzastore/dao/OrdineDAOImpl.java
+++ src/main/java/it/prova/pizzastore/dao/OrdineDAOImpl.java
@@ -1,9 +1,15 @@
 package it.prova.pizzastore.dao;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import org.apache.commons.lang3.StringUtils;
 
 import it.prova.pizzastore.model.Ordine;
 
@@ -46,6 +52,34 @@ public class OrdineDAOImpl implements OrdineDAO {
 
 	public void setEntityManager(EntityManager entityManager) {
 		this.entityManager = entityManager;
+	}
+	
+	public List<Ordine> findByExample(Ordine example) throws Exception{
+		
+		Map<String, Object> paramaterMap = new HashMap<String, Object>();
+		List<String> whereClauses = new ArrayList<String>();
+
+		StringBuilder queryBuilder = new StringBuilder("select o from Ordine o where o.id = o.id ");
+		
+		if(StringUtils.isNotEmpty(example.getCodice())) {
+			whereClauses.add(" o.codice  like :codice ");
+			paramaterMap.put("codice", "%" + example.getCodice() + "%");
+		}
+		if(example.getData() != null) {
+			whereClauses.add(" o.data >= :data");
+			paramaterMap.put("data", example.getData());
+		}
+		
+		queryBuilder.append(!whereClauses.isEmpty()?" and ":"");
+		queryBuilder.append(StringUtils.join(whereClauses, " and "));
+		
+		TypedQuery<Ordine> typedQuery = entityManager.createQuery(queryBuilder.toString(), Ordine.class);
+
+		for (String key : paramaterMap.keySet()) {
+			typedQuery.setParameter(key, paramaterMap.get(key));
+		}
+
+		return typedQuery.getResultList();
 	}
 
 }
