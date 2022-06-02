e4d180baab661d6338ade45f969c60976222ef9f
AlessioConti <alessioconti02@yahoo.it>
2022-06-02 11:22:39 +0200

    Implementazione createClienteParas + validateCliente

--- src/main/java/it/prova/pizzastore/utility/UtilityForm.java
+++ src/main/java/it/prova/pizzastore/utility/UtilityForm.java
@@ -6,6 +6,7 @@ import java.util.Date;
 
 import org.apache.commons.lang3.StringUtils;
 
+import it.prova.pizzastore.model.Cliente;
 import it.prova.pizzastore.model.Pizza;
 
 public class UtilityForm {
@@ -19,6 +20,17 @@ public class UtilityForm {
 	public static boolean validatePizzaBean(Pizza pizzaToBeValidated) {
 		if (StringUtils.isBlank(pizzaToBeValidated.getDescrizione())
 				|| StringUtils.isBlank(pizzaToBeValidated.getIngredienti()) || pizzaToBeValidated.getPrezzoBase() == 0)
+			return false;
+		return true;
+	}
+	
+	public static Cliente createClienteFromParas(String nomeParas, String cognomeParas, String indirizzoParas) {
+		Cliente result =new Cliente(nomeParas, cognomeParas, indirizzoParas);
+		return result;
+	}
+	
+	public static boolean validateClienteBean(Cliente clienteToBeValidated) {
+		if(StringUtils.isBlank(clienteToBeValidated.getNome()) || StringUtils.isBlank(clienteToBeValidated.getCognome()) || StringUtils.isAllBlank(clienteToBeValidated.getIndirizzo()))
 			return false;
 		return true;
 	}
